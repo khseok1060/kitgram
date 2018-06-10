7bec27b48d29f7aa54ed11bfa645e4454f9e529f
Kwangho <khseok1060@gmail.com>
2018-06-10 16:58:35 +0900

    1-25 Customizing the Django Admin

--- kitgram/users/models.py
+++ kitgram/users/models.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class User(AbstractUser):
 
     """User Model"""
