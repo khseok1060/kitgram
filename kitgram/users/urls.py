159312b3bed5d130c2133880656507df971526e9
Kwangho <khseok1060@gmail.com>
2018-06-16 20:41:19 +0900

    1-57 Search users by username

--- kitgram/users/urls.py
+++ kitgram/users/urls.py
@@ -19,11 +19,6 @@ urlpatterns = [
     name='unfollow_user',
   ),
   url(
-    regex=r'^(?P<username>\w+)/$',
-    view=views.UserProfile.as_view(),
-    name="user_profile"
-  ),
-  url(
     regex=r'^(?P<username>\w+)/followers/$',
     view=views.UserFollowers.as_view(),
     name='user_followers',
@@ -32,5 +27,15 @@ urlpatterns = [
     regex=r'^(?P<username>\w+)/following/$',
     view=views.UserFollowing.as_view(),
     name='user_following',
+  ),
+  url(
+    regex=r'^search/$',
+    view=views.Search.as_view(),
+    name='search', #url 순서변경을 통해 search부터 실행하도록 함. 아래 것이 위에 있으면 쿼리에 username으로 인식되어 userprofile을 찾는다
+  ),
+  url(
+    regex=r'^(?P<username>\w+)/$',
+    view=views.UserProfile.as_view(),
+    name="user_profile"
   ),
 ]
