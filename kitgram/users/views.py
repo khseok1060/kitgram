6be9255fea06fa30284bec30b45c0a19ab2737b5
Kwangho <khseok1060@gmail.com>
2018-06-22 20:20:55 +0900

    1-67 Update Profile

--- kitgram/users/views.py
+++ kitgram/users/views.py
@@ -7,113 +7,152 @@ from kitgram.notifications import views as notification_views
 
 class ExploreUsers(APIView):
 
-  def get(self, request, format=None):
+    def get(self, request, format=None):
 
-    last_five = models.User.objects.all().order_by('-date_joined')[:5]
+        last_five = models.User.objects.all().order_by('-date_joined')[:5]
 
-    serializer = serializers.ListUserSerializer(last_five, many=True)
+        serializer = serializers.ListUserSerializer(last_five, many=True)
 
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class FollowUser(APIView):
 
-  def post(self, request, user_id, format=None):
+    def post(self, request, user_id, format=None):
 
-    user = request.user
+        user = request.user
 
-    try:
-      user_to_follow = models.User.objects.get(id=user_id)
-    except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            user_to_follow = models.User.objects.get(id=user_id)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    user.following.add(user_to_follow)
+        user.following.add(user_to_follow)
 
-    user.save()
+        user.save()
 
-    notification_views.create_notification(user, user_to_follow, 'follow')
+        notification_views.create_notification(user, user_to_follow, 'follow')
 
-    return Response(status=status.HTTP_200_OK) 
+        return Response(status=status.HTTP_200_OK) 
 
 
 class UnFollowUser(APIView):
 
-  def post(self, request, user_id, format=None):
+    def post(self, request, user_id, format=None):
 
-    user = request.user
+        user = request.user
 
-    try:
-      user_to_follow = models.User.objects.get(id=user_id)
-    except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            user_to_follow = models.User.objects.get(id=user_id)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    user.following.remove(user_to_follow)
+        user.following.remove(user_to_follow)
 
-    user.save()
+        user.save()
 
-    return Response(status=status.HTTP_200_OK) 
+        return Response(status=status.HTTP_200_OK) 
 
 
 class UserProfile(APIView):
 
-  def get(self, request, username, format=None):
+    def get_user(self, username):
 
-    try:
-      found_user = models.User.objects.get(username=username)
-    except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            found_user = models.User.objects.get(username=username)
+            return found_user
+        except models.User.DoesNotExist:
+            return None
 
-    serializer = serializers.UserProfileSerializer(found_user)
 
-    return Responese(data=serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, username, format=None):
+
+        found_user = self.get_user(self, username)
+
+        if found_user is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.UserProfileSerializer(found_user)
+
+        return Responese(data=serializer.data, status=status.HTTP_200_OK)
+
+
+    def put(self, request, username, format=None):
+
+        user = request.user
+
+        found_user = self.get_user(self, username)
+
+        if found_user is None:
+            
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        elif found_user.username != user.username:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        else:
+
+            serializer = serializers.UserProfileSerializer(found_user, data=request.data, partial=True)
+
+            if serializer.is_valid():
+
+                serializer.save()
+            
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            
+            else:
+
+                return Response(daat=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserFollowers(APIView):
 
-  def get(self, request, username, format=None):
+    def get(self, request, username, format=None):
 
-    try:
-      found_user = models.User.objects.get(username=username)
-    except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            found_user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    user_followers = found_user.followers.all()
+        user_followers = found_user.followers.all()
 
-    serializer = serializers.ListUserSerializer(user_followers, many=True)
+        serializer = serializers.ListUserSerializer(user_followers, many=True)
 
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class UserFollowing(APIView):
 
-  def get(self, request, username, format=None):
+    def get(self, request, username, format=None):
 
-    try:
-      found_user = models.User.objects.get(username=username)
-    except models.User.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            found_user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    user_following = found_user.following.all()
+        user_following = found_user.following.all()
 
-    serializer = serializers.ListUserSerializer(user_following, many=True)
+        serializer = serializers.ListUserSerializer(user_following, many=True)
 
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class Search(APIView):
 
-  def get(self, request, format=None):
+    def get(self, request, format=None):
 
-    username = request.query_params.get('username', None)
+        username = request.query_params.get('username', None)
 
-    if username is not None:
+        if username is not None:
 
-      users = models.User.objects.filter(username__icontains=username)
+            users = models.User.objects.filter(username__icontains=username)
 
-      serializer = serializers.ListUserSerializer(users, many=True)
+            serializer = serializers.ListUserSerializer(users, many=True)
 
-      return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    else:
+        else:
 
-      return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
