fefe6bdbaf2e671da3ff1f4a4e3c7fbf67990c38
Kwangho <khseok1060@gmail.com>
2018-06-25 20:05:59 +0900

    3-24 Auth Component part One

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Switch } from "react-router-dom";
-import styles from './styles.scss';
+import './styles.scss';
 import Footer from 'components/Footer';
+import Auth from 'components/Auth';
 
 const App  = props => [
   // Nav,
@@ -12,16 +14,20 @@ const App  = props => [
   <Footer key={3} />
 ];
 
+App.PropTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+}
+
 const PrivateRoutes = props => (
   <Switch>
-    <Route exact path="/" render={() => "feed"} />
+    <Route exact path="/" render={() => "feed"} />,
     <Route exact path="/explore" render={() => "explore"} />
   </Switch>
 );
 
 const PublicRoutes = props => (
   <Switch>
-    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/" component={Auth} />,
     <Route exact path="/forgot" render={() => "password"} />
   </Switch>
 );
---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Add all the actions for:
+// Log in
+// Sign up
+// Recover Password
+// Check username
+// Check Password
+// Check Email
+
+export default connect()(Container);
