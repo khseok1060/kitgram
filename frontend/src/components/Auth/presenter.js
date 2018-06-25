c7fd669b1f372343511666175dce5777c650639f
Kwangho <khseok1060@gmail.com>
2018-06-26 00:14:41 +0900

    3-30 Stateless Components, Context and Translation

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./styles.scss";
 import { LoginForm, SignupForm } from "components/AuthForms";
 
@@ -19,22 +20,22 @@ const Auth = (props, context) => (
       </div>
       <div className={styles.whiteBox}>
         {props.action === "login" && (
-          <p className={styles.text}>Don't have an account?{" "}
+          <p className={styles.text}>{context.t("Don't have an account?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              Sign up
+              {context.t("Sign up")}
             </span>
           </p>)
         }
         {props.action === "signup" && (
-          <p className={styles.text}>Have an account?{" "}
+          <p className={styles.text}>{context.t("Have an account?")}{" "}
             <span onClick={props.changeAction} className={styles.changeLink}>
-              Log in
+              {context.t("Log in")}
             </span>
           </p>)
         }
       </div>
       <div className={styles.appBox}>
-        <span>Get the app</span>
+        <span>{context.t("Get the app")}</span>
         <div className={styles.appstores}>
           <img
             src={require("images/ios.png")}
@@ -50,5 +51,9 @@ const Auth = (props, context) => (
     </div>
   </main>
 );
+
+Auth.contextTypes = {
+  t: PropTypes.func.isRequired
+}
 
 export default Auth;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,26 @@
 export const translations = {
   kr: {
-    "Login in": "로그인"
+    "Log in": "로그인",
+    "About Us": "INSTAGRAM 정보",
+    "Support": "지원",
+    "Blog": "블로그",
+    "Press": "홍보 센터",
+    "Jobs": "채용 정보",
+    "Privacy": "개인정보처리방침" ,
+    "Terms": "약관",
+    "Directory": "디렉토리",
+    "Language": "언어",
+    "Username": "사용자 이름",
+    "Password": "비밀번호",
+    "or": "또는",
+    "Log in with Facebook": "Facebook으로 로그인하기",
+    "Forgot password": "비밀번호를 잊어버리셨나요",
+    "Sign up to see photos and videos from your friends": "친구들의 사진과 동영상을 보려면 가입하세요",
+    "Sign up": "가입",
+    "Email": "이메일 주소",
+    "Full name": "성명",
+    "Don't have an account?": "계정이 없으신가요?",
+    "Have an account?": "계정이 있으신가요?",
+    "Get the app": "앱을 다운로드하세요"
   }
 }
