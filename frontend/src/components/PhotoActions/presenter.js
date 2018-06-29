e7dbbc4a220eb77a4d3e0ab26b45c84e16235ea9
Kwangho <khseok1060@gmail.com>
2018-06-29 21:30:57 +0900

    CSS styles & Responsive

--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -4,25 +4,28 @@ import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
 const PhotoActions = (props, context) => (
-  <div>
-    <div>
-      <span>
+  <div className={styles.actions}>
+    <div className={styles.icons}>
+      <span className={styles.icon}>
         <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
       </span>
-      <span>
+      <span className={styles.icon}>
         <Ionicon icon="ios-text-outline" fontSize="28px" color="black" />
       </span>
     </div>
-    <span>{props.number} {props.number === 1 ? context.t("like") : context.t("likes")}</span>
+    <span className={styles.likes}>
+      {props.number}{" "}
+      {props.number === 1 ? context.t("like") : context.t("likes")}
+    </span>
   </div>
 );
 
-PhotoActions.contextTypes = {
-  t: PropTypes.func.isRequired
-}
-
 PhotoActions.propTypes = {
   number: PropTypes.number.isRequired
-}
+};
+
+PhotoActions.contextTypes = {
+  t: PropTypes.func.isRequired
+};
 
 export default PhotoActions;
--- frontend/src/components/TimeStamp/index.js
+++ frontend/src/components/TimeStamp/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
-const TimeStamp = (props, context) => props.time;
+const TimeStamp = (props, context) => (
+  <span className={styles.time}>{props.time}</span>
+);
+
+TimeStamp.contextTypes = {
+  t: PropTypes.func.isRequired
+}
 
 TimeStamp.propTypes = {
   time: PropTypes.string.isRequired
