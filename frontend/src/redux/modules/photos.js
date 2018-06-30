4eeb5ad0ed36d0dc8354efcb0b747aa61b4fbe38
Kwangho <khseok1060@gmail.com>
2018-06-30 15:32:19 +0900

    3-55 Liking Photos pt. One

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -3,12 +3,28 @@ import { actionCreators as userActions} from "redux/modules/user";
 
 // actions
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const UNLIKE_PHOTO = "UNLIKE_PHOTO";
 
 // action creators
 function setFeed(feed) {
   return {
     type: SET_FEED,
     feed
+  }
+}
+
+function doLikePhoto(photoId) {
+  return {
+    type: LIKE_PHOTO,
+    photoId
+  }
+}
+
+function doUnlikePhoto(photoId) {
+  return {
+    type: UNLIKE_PHOTO,
+    photoId
   }
 }
 
@@ -30,7 +46,13 @@ function getFeed() {
     .then(json => dispatch(setFeed(json)))
     .catch(err => console.log(err))
   }
-};
+}
+
+function likePhoto(photoId) {
+  return (dispatch, getState) => {
+    dispatch(doLikePhoto(photoId))
+  }
+} 
 
 
 // initial state
@@ -41,6 +63,10 @@ function reducer(state=initialState, action) {
   switch(action.type){
     case SET_FEED:
       return applySetFeed(state, action);
+    case LIKE_PHOTO:
+      return applyLikePhoto(state, action);
+    case UNLIKE_PHOTO:
+      return applyUnlikePhoto(state, action);
     default:
       return state;
   }
@@ -54,6 +80,31 @@ function applySetFeed(state, action) {
     feed
   };
 };
+
+function applyLikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if(photo.id === photoId) {
+      return { ...photo, is_liked: true, like_count: photo.like_count + 1};
+    }
+    return photo;
+  });
+  return { ...state, feed: updatedFeed };
+}
+
+function applyUnlikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+  const updatedFeed = feed.map(photo => {
+    if(photo.id === photoId) {
+      return { ...photo, is_liked: false, like_count: photo.like_count - 1};
+    }
+    return photo;
+  });
+  return { ...state, feed: updatedFeed };
+  
+}
 
 // exports
 const actionCreators = {
