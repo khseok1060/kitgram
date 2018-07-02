216ca63289791ab12beb8a8efa5f937ce09a3889
Kwangho <khseok1060@gmail.com>
2018-07-02 20:38:20 +0900

    3-66 Explore

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -172,6 +172,25 @@ function unfollowUser(userId) {
   }
 }
 
+function getExplore() {
+  return (dispatch, getState) => {
+    const { user: { token } } = getState();
+    fetch(`/users/explore/`, {
+      method: "GET",
+      headers: {
+        Authorization: `JWT ${token}`      
+      }
+    })
+    .then(response => {
+      if(response.status === 401) {
+        dispatch(logout());
+      }
+      return response.json()
+    })
+    .then(json => dispatch(setUserList(json)));
+  }
+}
+
 // initial state
 const initialState = {
   isLoggedIn: localStorage.getItem('jwt') ? true : false,
@@ -255,7 +274,8 @@ const actionCreators = {
   logout,
   getPhotoLikes,
   followUser,
-  unfollowUser
+  unfollowUser,
+  getExplore
 };
 
 export { actionCreators };
