dcbc213fda7a1f948f8a7c521c92b41925105cb8
Kwangho <khseok1060@gmail.com>
2018-07-02 20:03:49 +0900

    3-64 Follow Unfollow User Offline

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -3,6 +3,8 @@
 const SAVE_TOKEN = "SAVE_TOKEN";
 const LOGOUT = "LOGOUT";
 const SET_USER_LIST = "SET_USER_LIST";
+const FOLLOW_USER = "FOLLOW_USER";
+const UNFOLLOW_USER = "UNFOLLOW_USER";
 
 // action creators
 function saveToken(token) {
@@ -22,6 +24,20 @@ function setUserList(userList) {
   return {
     type: SET_USER_LIST,
     userList
+  };
+}
+
+function setFollowUser(userId) {
+  return {
+    type: FOLLOW_USER,
+    userId
+  };
+}
+
+function setUnfollowUser(userId) {
+  return {
+    type: UNFOLLOW_USER,
+    userId
   };
 }
 
@@ -114,6 +130,18 @@ function getPhotoLikes(photoId) {
   }
 }
 
+function followUser(userId) {
+  return (dispatch, getState) => {
+    dispatch(setFollowUser(userId));
+  }
+}
+
+function unfollowUser(userId) {
+  return (dispatch, getState) => {
+    dispatch(setUnfollowUser(userId));
+  }
+}
+
 // initial state
 const initialState = {
   isLoggedIn: localStorage.getItem('jwt') ? true : false,
@@ -129,6 +157,10 @@ function reducer(state = initialState, action) {
       return applyLogout(state, action);
     case SET_USER_LIST:
       return applySetUserList(state, action);
+    case FOLLOW_USER:
+      return applyFollowUser(state, action);
+    case UNFOLLOW_USER:
+      return applyUnfollowUser(state, action);
     default:
       return state;
   }
@@ -161,13 +193,39 @@ function applySetUserList(state, action) {
   };
 }
 
+function applyFollowUser(state, action) {
+  const { userId } = action;
+  const { userList } = state;
+  const updatedUserList = userList.map(user => {
+    if(user.id === userId) {
+      return { ...user, following: true }
+    }
+    return user;
+  });
+  return {...state, userList: updatedUserList}
+}
+
+function applyUnfollowUser(state, action) {
+  const { userId } = action;
+  const { userList } = state;
+  const updatedUserList = userList.map(user => {
+    if(user.id === userId) {
+      return { ...user, following: false }
+    }
+    return user;
+  });
+  return {...state, userList: updatedUserList}
+}
+
 // exports
 const actionCreators = {
   facebookLogin,
   usernameLogin,
   createAccount,
   logout,
-  getPhotoLikes
+  getPhotoLikes,
+  followUser,
+  unfollowUser
 };
 
 export { actionCreators };
